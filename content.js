4fda14c331ac6ed14be37282350669626807ac69
Fabio Romariz <romariz@gmail.com>
2020-12-20 20:11:36 -0300

    Adjustments and improvements

--- content.js
+++ content.js
@@ -1,22 +1,16 @@
-var sportsEls = document.getElementsByClassName("hui-color-sports");
-if (sportsEls && sportsEls.length > 0) {
-  for (var i = 0; i < sportsEls.length - 1; i++) {
-    sportsEls[i].style.display = "none";
-  }
-}
+hideStuff = () => {
+  $(".hui-color-sports").css("display", "none");
+  $(".slick-slide").css("display", "none");
+  $(".hui-publicidade--banner_slb_topo").css("display", "none");
+  $(".hui-publicidade--has-divider").css("display", "none");
+  $(".hui-publicidade--banner_slb_fim").css("display", "none");
+  $(".home-widgets-inferiores").css("display", "none");
+  $(".home-topglobocom").css("display", "none");
+  $(".medium-8").addClass("medium-12").removeClass("medium-8");
+  $(".hui-highlight--with-separator").css("min-height", "67px");
+  $(".hui-premium-multiple__container").css("display", "inline");
+};
 
-var slickSlides = document.getElementsByClassName("slick-slide");
-if (slickSlides && slickSlides.length > 0) {
-  for (var i = 0; i < slickSlides.length - 1; i++) {
-    if (slickSlides[i].getAttribute("data-slick-index") == 1) {
-      slickSlides[i].style.visibility = "hidden";
-    }
-  }
-}
-
-var publis = document.getElementsByClassName("hui-publicidade");
-if (publis && publis.length > 0) {
-  for (var i = 0; i < slickSlides.length - 1; i++) {
-    slickSlides[i].style.display = "none";
-  }
-}
+$(document).ready(function () {
+  document.addEventListener("load", hideStuff());
+});
