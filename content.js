4776e8760a82d72bf5803e17b5afe6deffd0357d
Fabio Romariz <romariz@gmail.com>
2021-01-27 13:09:44 -0300

    Added more hidden controls

--- content.js
+++ content.js
@@ -13,20 +13,8 @@ hideStuff = () => {
     "78.1% calc((100% - 75.1%) - 38.35px)"
   );
   $(".highlight-container").css("grid-template-areas", '"e b" "c b" "d b"');
-  console.log(
-    "The grid-template area should have been redefined now. The blue block should have move to the top row."
-  );
-
-  // $(".hui-color-sports").css("display", "none");
-  // $(".slick-slide").css("display", "none");
-  // $(".hui-publicidade--banner_slb_topo").css("display", "none");
-  // $(".hui-publicidade--has-divider").css("display", "none");
-  // $(".hui-publicidade--banner_slb_fim").css("display", "none");
-  // $(".home-widgets-inferiores").css("display", "none");
-  // $(".home-topglobocom").css("display", "none");
-  // $(".medium-8").addClass("medium-12").removeClass("medium-8");
-  // $(".hui-highlight--with-separator").css("min-height", "67px");
-  // $(".hui-premium-multiple__container").css("display", "inline");
+  $(".homeui-tc-esporte").hide();
+  $(".header-titles").css("grid-template-columns", "repeat(4,auto)");
 };
 
 $(document).ready(function () {
